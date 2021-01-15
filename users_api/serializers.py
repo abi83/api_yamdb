8558bbd7038b5fcca31bbce3e97c6c13375c894e
abi83 <GFhjkm83>
2021-01-15 22:05:33 +0700

    working with serializers

--- users_api/serializers.py
+++ users_api/serializers.py
@@ -2,10 +2,38 @@ from users_api.models import YamdbUser
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class MeSerializer(serializers.ModelSerializer):
+    # url = serializers.ReadOnlyField(source='username', read_only=True)
+    username = serializers.ReadOnlyField()
 
     class Meta:
         model = YamdbUser
-        fields = ['username', 'first_name', 'last_name', 'email', 'bio', 'role']
+        fields = ['username', 'first_name', 'last_name', 'email', 'bio', 'role',
+                  # 'url',
+                  ]
         lookup_field = 'username'
-        extra_kwargs = {'url': {'lookup_field': 'username'}}
+        # extra_kwargs = {'url': {'lookup_field': 'username'}}
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        """Convert `username` to lowercase."""
+        ret = super().to_representation(instance)
+        ret['role'] = ret['role']
+        return ret
+
+
+    # def get_role(self, obj):
+    #     breakpoint()
+    #     return obj.role[9:].split("'")[0]
+    # role2 = serializers.ChoiceField(choices=YamdbUser.CHOICES)
+    # role = serializers.SerializerMethodField(read_only=True, source='get_role')
+
+    class Meta:
+        model = YamdbUser
+        fields = ['username', 'first_name', 'last_name', 'email', 'bio', 'role',
+                  ]
+        lookup_field = 'username'
+
+

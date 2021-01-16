7b7035b332b9e75d8493c812a9b26f87cc7fdee3
abi83 <GFhjkm83>
2021-01-17 02:24:48 +0700

    permission changes

--- title_api/permissions.py
+++ title_api/permissions.py
@@ -13,8 +13,8 @@ class AuthorPermissions(permissions.BasePermission):
         return True
 
 
-class AdminPermissions(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method not in permissions.SAFE_METHODS:
-            return request.user == obj.author
-        return True
+# class AdminPermissions(permissions.BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         if request.method not in permissions.SAFE_METHODS:
+#             return request.user == obj.author
+#         return True
--- users_api/permissions.py
+++ users_api/permissions.py
@@ -8,3 +8,11 @@ class IsYamdbAdmin(permissions.BasePermission):
             return request.user.is_superuser or request.user.role == str(YamdbUser.ADMIN)
 
         return False
+
+
+class IsYamdbModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.is_superuser or request.user.role == str(YamdbUser.MODERATOR)
+
+        return False

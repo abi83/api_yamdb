d0de6db9935c8885e5a9de7552a7513eb795be5b
abi83 <GFhjkm83>
2021-01-17 18:27:40 +0700

    10 from 11 passed in users

--- users_api/permissions.py
+++ users_api/permissions.py
@@ -4,6 +4,7 @@ from users_api.models import YamdbUser
 
 class IsYamdbAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
+        # breakpoint()
         if request.user.is_authenticated:
             return request.user.is_superuser or request.user.role == str(YamdbUser.ADMIN)
 

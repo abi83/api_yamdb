7006880293120a176a866a7701af9098c0d183cd
abi83 <GFhjkm83>
2021-03-13 13:39:09 +0700

    CORS allowed all

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://212.164.39.208",
